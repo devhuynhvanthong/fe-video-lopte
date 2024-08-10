05aebcb8755617edf9c7c052373d218b3bd23a42
Dev.Huỳnh Thông <dev.huynhvanthong@gmail.com>
2024-08-11 01:24:47 +0700

    no message

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {resolve} from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3017,
+    historyApiFallback: true,
   },
   define: {
     "process.env": {},
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
 });
